0ed69aa6e7353041a042e887ef170fc9deb35289
Raul Pereira <raul.pereira@gmail.com>
2026-04-26 22:49:27 -0300

    test(avatar_live): add rendering tests for avatar URL, HTML, image, and element

--- test/ranger_web/live/avatar_live_test.exs
+++ test/ranger_web/live/avatar_live_test.exs
@@ -2,4 +2,42 @@ defmodule RangerWeb.AvatarLiveTest do
   use RangerWeb.ConnCase
 
   import Phoenix.LiveViewTest
+
+  alias Ranger.Gravatar
+
+  test "renders avatar for give email", ctx do
+    email = "frodo@shire.com"
+    avatar_url = Gravatar.generate(email)
+    {:ok, _view, html} = live(ctx.conn, ~p"/avatar/#{email}")
+
+    assert html =~ avatar_url
+  end
+
+  test "renders avatar HTML", ctx do
+    email = "frodo@shire.com"
+    avatar_url = Gravatar.generate(email)
+    {:ok, _view, html} = live(ctx.conn, ~p"/avatar/#{email}")
+
+    avatar = ~s(<img class="avatar" src="#{avatar_url}")
+
+    assert html =~ avatar
+  end
+
+  test "renders avatar image for given email", ctx do
+    email = "frodo@shire.com"
+    avatar_url = Gravatar.generate(email)
+    {:ok, view, _html} = live(ctx.conn, ~p"/avatar/#{email}")
+
+    assert has_element?(view, ~s(img[src*="#{avatar_url}"]))
+  end
+
+  test "renders avatar element for given email", ctx do
+    email = "frodo@shire.com"
+    avatar_url = Gravatar.generate(email)
+    {:ok, view, _html} = live(ctx.conn, ~p"/avatar/#{email}")
+
+    avatar = element(view, ~s(img[src*="#{avatar_url}"]))
+
+    assert has_element?(avatar)
+  end
 end
