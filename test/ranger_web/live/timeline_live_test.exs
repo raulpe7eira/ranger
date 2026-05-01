0967639e5d02bff658013e4477a95f295087cd99
Raul Pereira <raul.pereira@gmail.com>
2026-05-01 10:53:37 -0300

    test(timeline_live): add infinite scroll pagination test

--- test/ranger_web/live/timeline_live_test.exs
+++ test/ranger_web/live/timeline_live_test.exs
@@ -4,4 +4,16 @@ defmodule RangerWeb.TimelineLiveTest do
   import Phoenix.LiveViewTest
 
   alias Ranger.Timeline
+
+  test "user can see older posts with infinite scroll", ctx do
+    [post] = Timeline.posts(offset: 10, limit: 1)
+    {:ok, view, _html} = live(ctx.conn, ~p"/timeline")
+
+    view
+    |> element("#load-more", "Loading ...")
+    |> render_hook("load-more")
+
+    assert has_element?(view, "[data-role=author]", post.author)
+    assert has_element?(view, "[data-role=post]", post.text)
+  end
 end
