7ea5e5d05d1c10a3b945d8d75a3a1145148404d4
Raul Pereira <raul.pereira@gmail.com>
2026-04-30 21:53:18 -0300

    test(counter_live): add increment interaction tests using click and event

--- test/ranger_web/live/counter_live_test.exs
+++ test/ranger_web/live/counter_live_test.exs
@@ -2,4 +2,33 @@ defmodule RangerWeb.CounterLiveTest do
   use RangerWeb.ConnCase
 
   import Phoenix.LiveViewTest
+
+  test "user con increase counter", ctx do
+    {:ok, view, _html} = live(ctx.conn, ~p"/counter")
+
+    view
+    |> element("#increment")
+    |> render_click()
+
+    assert has_element?(view, "#count", "1")
+  end
+
+  test "user con increase counter (uses HTML)", ctx do
+    {:ok, view, _html} = live(ctx.conn, ~p"/counter")
+
+    html =
+      view
+      |> element("#increment")
+      |> render_click()
+
+    assert html =~ "1"
+  end
+
+  test "user con increase counter (target event directly)", ctx do
+    {:ok, view, _html} = live(ctx.conn, ~p"/counter")
+
+    render_click(view, "increase")
+
+    assert has_element?(view, "#count", "1")
+  end
 end
