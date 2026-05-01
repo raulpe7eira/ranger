ee71514ed8707a9ddbe5a8410cb4362a8ac7c12f
Raul Pereira <raul.pereira@gmail.com>
2026-04-30 22:36:00 -0300

    test(todo_live): add todo creation tests

--- test/ranger_web/live/todo_live_test.exs
+++ test/ranger_web/live/todo_live_test.exs
@@ -2,4 +2,33 @@ defmodule RangerWeb.TodoLiveTest do
   use RangerWeb.ConnCase
 
   import Phoenix.LiveViewTest
+
+  test "user can create a new todo", ctx do
+    {:ok, view, _html} = live(ctx.conn, ~p"/todo")
+
+    view
+    |> form("#add-todo", %{todo: %{body: "Form fellowship"}})
+    |> render_submit()
+
+    assert has_element?(view, "[data-role=todo]", "Form fellowship")
+  end
+
+  test "user can create a new todo (target event directly)", ctx do
+    {:ok, view, _html} = live(ctx.conn, ~p"/todo")
+
+    render_submit(view, "create", %{todo: %{body: "Form fellowship"}})
+
+    assert has_element?(view, "[data-role=todo]", "Form fellowship")
+  end
+
+  test "user can create a new todo (with HTML)", ctx do
+    {:ok, view, _html} = live(ctx.conn, ~p"/todo")
+
+    html =
+      view
+      |> form("#add-todo", %{todo: %{body: "Form fellowship"}})
+      |> render_submit()
+
+    assert html =~ "Form fellowship"
+  end
 end
