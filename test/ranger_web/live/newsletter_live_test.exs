2150e9f779cbba971cb3b69643a23bec5d30ff87
Raul Pereira <raul.pereira@gmail.com>
2026-04-30 22:48:20 -0300

    test(newsletter_live): add email validation warning tests

--- test/ranger_web/live/newsletter_live_test.exs
+++ test/ranger_web/live/newsletter_live_test.exs
@@ -2,4 +2,25 @@ defmodule RangerWeb.NewsletterLiveTest do
   use RangerWeb.ConnCase
 
   import Phoenix.LiveViewTest
+
+  test "warns user of invalid email as user types", ctx do
+    {:ok, view, _html} = live(ctx.conn, ~p"/newsletter")
+    invalid_email = "something"
+
+    html =
+      view
+      |> form("#subscribe", %{subscription: %{email: invalid_email}})
+      |> render_change()
+
+    assert html =~ "has invalid format"
+  end
+
+  test "warns user of invalid email as user types (target event directly)", ctx do
+    {:ok, view, _html} = live(ctx.conn, ~p"/newsletter")
+    invalid_email = "something"
+
+    html = render_change(view, "validate", %{subscription: %{email: invalid_email}})
+
+    assert html =~ "has invalid format"
+  end
 end
