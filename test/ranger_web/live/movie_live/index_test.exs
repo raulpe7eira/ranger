3d67023b10cf5a755be2ef20feaf8d4bee4a05cc
Raul Pereira <raul.pereira@gmail.com>
2026-05-01 13:02:56 -0300

    test(movie_live): add upload preview, cancel, validation, preflight, and URL storage tests

--- test/ranger_web/live/movie_live/index_test.exs
+++ test/ranger_web/live/movie_live/index_test.exs
@@ -3,17 +3,77 @@ defmodule RangerWeb.MovieLive.IndexTest do
 
   import Phoenix.LiveViewTest
 
-  test "user can create movie", %{conn: conn} do
-    {:ok, view, _html} = live(conn, ~p"/movies")
+  alias Ranger.CloudinaryUpload
+
+  test "user can see preview of poster to be uploaded", ctx do
+    {:ok, view, _html} = live(ctx.conn, ~p"/movies")
+
+    assert view
+           |> upload("fellowship-poster.jpg")
+           |> has_element?("[data-role='image-preview']")
+  end
+
+  test "user can cancel upload", ctx do
+    {:ok, view, _html} = live(ctx.conn, ~p"/movies")
+
+    view
+    |> upload("fellowship-poster.jpg")
+    |> cancel_upload()
+
+    refute has_element?(view, "[data-role='image-preview']")
+  end
+
+  test "user sees error when uploading too many files", ctx do
+    {:ok, view, _html} = live(ctx.conn, ~p"/movies")
+
+    view
+    |> upload("fellowship-poster.jpg")
+    |> upload("fellowship-poster.jpg")
+    |> upload("fellowship-poster.jpg")
+
+    assert render(view) =~ "Too many files"
+  end
+
+  test "generates correct metadata for external upload", ctx do
+    {:ok, view, _html} = live(ctx.conn, ~p"/movies")
+
+    {:ok, %{entries: entries}} =
+      view
+      |> file_input("#upload-form", :posters, [
+        %{
+          name: "fellowship-poster.jpg",
+          content: File.read!("test/support/images/fellowship-poster.jpg"),
+          type: "image/jpeg"
+        }
+      ])
+      |> preflight_upload()
+
+    for {_k, v} <- entries do
+      assert v.uploader == "Cloudinary"
+      assert v.url =~ CloudinaryUpload.image_api_url(cloud_name())
+      assert v.fields[:folder] == "testing-liveview"
+
+      assert is_binary(v.fields[:public_id])
+      refute String.ends_with?(v.fields[:public_id], ".jpg")
+
+      assert is_binary(v.fields[:signature])
+      refute is_nil(v.fields[:api_key])
+      refute is_nil(v.fields[:timestamp])
+    end
+  end
+
+  test "user can create movie and stores correct URLs", ctx do
+    {:ok, view, _html} = live(ctx.conn, ~p"/movies")
 
     {:ok, show_view, _show_html} =
       view
       |> upload("fellowship-poster.jpg")
       |> create_movie("The Fellowship of the Ring")
-      |> follow_redirect(conn)
+      |> follow_redirect(ctx.conn)
 
     assert has_element?(show_view, "h2", "The Fellowship of the Ring")
-    assert has_element?(show_view, "[data-role='image']")
+    assert has_element?(show_view, "[data-role=image]")
+    assert hd(last_movie().poster_urls) =~ CloudinaryUpload.image_url(cloud_name())
   end
 
   defp upload(view, filename) do
@@ -34,9 +94,23 @@ defmodule RangerWeb.MovieLive.IndexTest do
     view
   end
 
+  defp cancel_upload(view) do
+    view
+    |> element("[data-role='cancel-upload']")
+    |> render_click()
+  end
+
   defp create_movie(view, name) do
     view
     |> form("#upload-form", %{movie: %{name: name}})
     |> render_submit()
+  end
+
+  defp cloud_name do
+    Application.get_env(:ranger, :cloudinary)[:cloud_name]
+  end
+
+  defp last_movie do
+    Ranger.Movie |> Ecto.Query.last() |> Ranger.Repo.one()
   end
 end
